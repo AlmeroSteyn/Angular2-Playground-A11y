1c203f7ffdaa98506c4c44fc82421c651eb217dd
AlmeroSteyn <almero.steyn@gmail.com>
2016-03-15 17:19:35 +0100

    Content

--- app/components/a11y-demo.component.ts
+++ app/components/a11y-demo.component.ts
@@ -4,11 +4,12 @@ import {ROUTER_DIRECTIVES} from "angular2/router";
 import {RouteConfig} from "angular2/router";
 import {A11yFailed} from "./a11y-failed.component";
 import {A11yPassed} from "./a11y-passed.component";
+import {HTTP_PROVIDERS} from "angular2/http";
 
 @Component({
   selector: 'a11y-demo',
   template: '<router-outlet></router-outlet>',
-  providers: [ROUTER_PROVIDERS],
+  providers: [ROUTER_PROVIDERS, HTTP_PROVIDERS],
   directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
