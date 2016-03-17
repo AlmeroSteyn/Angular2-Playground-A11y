9e71c08b4d84bbdf6fe30e523d87b9b34e2980fe
AlmeroSteyn <almero.steyn@gmail.com>
2016-03-17 06:44:58 +0100

    Content

--- app/components/a11y-passed-monster.component.ts
+++ app/components/a11y-passed-monster.component.ts
@@ -1,11 +1,23 @@
 import {Component} from "angular2/core";
 import {Input} from "angular2/core";
 import {IMonster} from "../models/monster.model";
+import {Monster} from "../models/monster.model";
+import {OnChanges} from "angular2/core";
 
 @Component({
   selector: 'a11y-passed-monster',
   templateUrl:'./app/components/a11y-passed-monster.component.html'
 })
-export class A11yPassedMonster{
+export class A11yPassedMonster implements OnChanges{
   @Input() monster: IMonster;
+
+  internalMonster: IMonster = new Monster();
+
+  ngOnChanges(changes:any):void {
+    var monsterChange:IMonster = changes.monster.currentValue;
+    if (monsterChange) {
+      this.internalMonster = new Monster(monsterChange.id, monsterChange.name, monsterChange.imageUrl,monsterChange.imageText);
+    }
+  }
+
 }
