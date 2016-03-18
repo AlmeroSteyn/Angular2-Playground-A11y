d397d4b3acc25792d40f2e4e8f5836b40435d718
AlmeroSteyn <almero.steyn@gmail.com>
2016-03-18 16:42:21 +0100

    Content

--- server/monsters.ts
+++ server/monsters.ts
@@ -3,24 +3,28 @@ module.exports = [
     id: 0,
     name: 'Moat Monster',
     imageUrl: '/images/l890291hjfdjwr832hrjdfswuer.jpg',
-    imageText: 'Moat monster attacking'
+    imageText: 'Moat monster attacking',
+    desc: 'This monster is mostly spotted in the area of the cookie tin. Just one moment of laxness and your month supply of delicacies will be gone!'
   },
   {
     id: 1,
     name: 'Hypno Cat',
     imageUrl: '/images/onfds9843hjksdf89432hkwjhrew.jpg',
-    imageText: 'Hypnotic cat in action'
+    imageText: 'Hypnotic cat in action',
+    desc: 'Do NOT make eye contact. The last person to do so is still sitting on a nest somewhere in the highest tree of the Amazon forest, making clucking sounds.'
   },
   {
     id: 2,
     name: 'Cat of Disaproval',
     imageUrl: '/images/fhgdsh8u98432frdsiur4583rewhsfeuwr.jpg',
-    imageText: 'The cat of disaproval in action'
+    imageText: 'The cat of disaproval in action',
+    desc: 'Had a successful week at work? This monster will suck your joy by scowling in the general direction of all your successes.'
   },
   {
     id: 3,
     name: 'Honeybadger',
     imageUrl: '/images/43247hrfjwh834924ihewuirewtrtrfe.jpg',
-    imageText: 'The honeybadger not caring'
+    imageText: 'The honeybadger not caring',
+    desc: 'Everyone knows what the honeybadger doesn\'t do. It is sure to make even the most staunch rebels amongst us look like blundering novices.'
   }
 ];
